7e7628d5823d1a841f1839e6de823d7436ba37e0
Ayazalidharejo <ayazali.xwave@gmail.com>
2025-06-07 23:33:37 +0500

    adding rounte

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173']
+  origin: ['http://localhost:5173','https://school-managment-frontend-web.vercel.app']
 }));
 
 app.use(express.json());
